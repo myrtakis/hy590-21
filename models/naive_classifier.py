268d675e59296bff0273d506375fe2f5ef5f65db
myrtakis <myrtakis@csd.uoc.gr>
2021-05-27 09:44:46 +0300

    Naive classifier completed

--- models/naive_classifier.py
+++ models/naive_classifier.py
@@ -5,18 +5,27 @@ import tensorflow as tf
 
 class Naive:
 
+    __losses__ = {
+        'mse': tf.losses.mean_squared_error,
+        'mae': tf.losses.mean_absolute_error
+    }
+
     def __init__(self):
         self.mean_per_neuron = None
         self.loss = None
+        self.metric = None
 
     def build_model(self, setting_configuration, window_config, model_params, input_dim, output_dim):
         self.loss = setting_configuration['loss']
+        self.metric = setting_configuration['metric'][0]
 
     def fit_model(self, Xtrain, Xval, epochs=20, callbacks=None):
         Xtrain_as_np = self.__convert_tensor_dataset_to_numpy__(Xtrain)
         # The window already takes care of the lag
         self.mean_per_neuron = pd.DataFrame(Xtrain_as_np).mean().values
-        history = {'loss': [], 'val_loss': self.evaluate(Xval)}
+        train_perf = self.evaluate(Xtrain)
+        val_perf = self.evaluate(Xval)
+        history = {'loss': train_perf[0], 'mae': train_perf[1], 'val_loss': val_perf[0], 'val_mae': val_perf[1]}
         return history
 
     def get_instance(self):
@@ -24,13 +33,10 @@ class Naive:
 
     def evaluate(self, data, verbose=None):
         tensor_dataset_as_np = self.__convert_tensor_dataset_to_numpy__(data)
-        if self.loss == 'mse':
-            return float(np.mean(tf.losses.mean_squared_error(tensor_dataset_as_np, self.mean_per_neuron).numpy()))
-        elif self.loss == 'mae':
-            return float(np.mean(tf.losses.mean_absolute_error(tensor_dataset_as_np, self.mean_per_neuron).numpy()))
-        else:
-            print('Uknown loss, None will be returned')
-            return None
+        return [
+            float(np.mean(Naive.__losses__[self.loss](tensor_dataset_as_np, self.mean_per_neuron).numpy())),
+            float(np.mean(Naive.__losses__[self.metric](tensor_dataset_as_np, self.mean_per_neuron).numpy()))
+        ]
 
     @staticmethod
     def __convert_tensor_dataset_to_numpy__(data):
