fixing normalization layer

core/pearson_corr_STTC_ANN_weights.py
@@ -21,7 +21,8 @@ import sys
 sys.exit()
 
 #os.chdir('C:\\Users\\john\\Desktop\\hy590-21\\environment\\configs\\fully_connected\\1622537625.328412\\'+fold_name)
-new_model = tf.keras.models.load_model('C:\\Users\\john\\Desktop\\hy590-21\\environment\\configs\\fully_connected\\1622048185.470065\\'+fold_name+'\\model_epoch_100.hdf5')
+
+new_model = tf.keras.models.load_model('D:\\Downloads\\fully_connected\\1622048185.470065\\'+fold_name+'\\model_epoch_100.hdf5')
 
 weights = new_model.weights[0]
    

models/fully_connected.py
@@ -40,9 +40,9 @@ class FullyConnected():
 
         #weights=[w,np.random.random(w.shape[1])],
 
+        model.add(tf.keras.layers.LayerNormalization(axis = -1))
+ 
         model.add(tf.keras.Input(shape=(input_dim,)))
-
-        model.add(tf.keras.layers.LayerNormalization())
 
         ### add hidden layers
         for units_per_layer in model_params['units_per_hidden_layer']: